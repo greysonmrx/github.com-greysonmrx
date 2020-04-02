4f0679144f606b7a8d37b10ad8e21e067043da95
unknown <greysonmrx@gmail.com>
2020-04-02 00:02:29 -0300

    feat: create decode effect

--- js/main.js
+++ js/main.js
@@ -12,5 +12,72 @@ function typeWriter(element) {
   });
 }
 
+function makeRandom(length) {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+function generateNames() {
+  let names = [];
+  let rounds = 18;
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(makeRandom(7));
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`g${makeRandom(6)}`);
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`gr${makeRandom(5)}`);
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`gre${makeRandom(4)}`);
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`grey${makeRandom(3)}`);
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`greys${makeRandom(2)}`);
+  }
+
+  for (let i = 0; i <= rounds - 1; i++) {
+    names.push(`greyso${makeRandom(1)}`);
+  }
+
+  names.push("greyson");
+
+  return names;
+}
+
+const arrayNames = generateNames();
+
+var index = 0;
+
+const decodeEffect = setInterval(() => {
+  document.getElementById("name").setAttribute("name", arrayNames[index]);
+  document.getElementById("name").innerHTML = arrayNames[index];
+
+  if (index === arrayNames.length - 1) {
+    clearInterval(decodeEffect);
+    setTimeout(() => {
+      document.getElementById("name").className = "glitch glitchEffect";
+    }, 250);
+  } else {
+    index++;
+  }
+}, 50);
+
 const title = document.getElementById("text");
 typeWriter(title);
